15c836d08bde92405d2332f8fd064cbab8385a6c
cihancalli <cihan.callii@gmail.com>
2022-03-14 23:24:54 +0300

    Change UI Label text with UI Button

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTabButton(_ sender: UIButton) {
+        //print("BUTTON TAPPED")
+        sender.setTitleColor(UIColor.green, for: UIControl.State.normal)
+        label.text = "Hoş, geldin!"
+    }
 }
 
